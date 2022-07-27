8eb9ad18e6f619b59cd025d768929a997a915db5
tzq0301 <1661610563@qq.com>
2022-07-28 01:09:04 +0800

    feat: 配置网关微服务

--- a9-gateway/src/main/java/a9/GatewayApplication.java
+++ a9-gateway/src/main/java/a9/GatewayApplication.java
@@ -2,12 +2,16 @@ package a9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author tzq0301
  * @version 1.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
