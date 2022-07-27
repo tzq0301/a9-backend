f936d4bb2aea6ad9d5433cf0b466c93086513ab8
tzq0301 <1661610563@qq.com>
2022-07-28 00:37:36 +0800

    feat: 完成 Nacos 配置

---
+++ a9-gateway/src/main/java/a9/GatewayApplication.java
@@ -0,0 +1,15 @@
+package a9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tzq0301
+ * @version 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
